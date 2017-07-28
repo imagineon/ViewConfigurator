a1a2d41a21bf8c6c2515ab7bbfb6e7c589cdb434
Peter Schumacher <ps@imagineon.de>
2017-07-28 20:35:35 +0200

    Added initial Configurator

---
+++ Sources/Core/Configurator.swift
@@ -0,0 +1,74 @@
+//
+//  Configurator.swift
+//  Configurator
+//
+//  Created by ImagineOn on 28.07.17.
+//  Copyright © 2017 ImagineOn GmbH. All rights reserved.
+//
+
+import Foundation
+
+protocol Configurateable: class {
+    init()
+}
+
+extension Configurateable {
+    static var configuration: ConfigurationSet<Self> {
+        return .init()
+    }
+    
+    func apply(from configurator: (ConfigurationSet<Self>) -> ConfigurationSet<Self>) -> Self {
+        return configurator(type(of: self).configuration).apply(self)
+    }
+
+    static func build(from configuraton: (ConfigurationSet<Self>) -> ConfigurationSet<Self>) -> Self {
+        return configuraton(self.configuration).build()
+    }
+}
+
+class ConfigurationSet<Base:Configurateable> {
+    typealias Configuration = (Base) -> Base
+    
+    private var configurations: [Configuration]
+    
+    fileprivate init() {
+        self.configurations = .init()
+    }
+    
+    func set(_ key: String, configurator block: @escaping Configuration) -> ConfigurationSet<Base> {
+        configurations.append(block)
+        
+        return self
+    }
+    
+    func set(_ key: String, configurator block: @escaping (Base) -> Void) -> ConfigurationSet<Base> {
+        return set(key) { (base) -> Base in
+            block(base)
+            
+            return base
+        }
+    }
+    
+    func without(_ key: String) -> ConfigurationSet<Base> {
+        remove(key)
+        
+        return self
+    }
+    
+    @discardableResult
+    func merge(_ configuration: ConfigurationSet<Base>) -> ConfigurationSet<Base> {
+        configurations.append(contentsOf: configuration.configurations)
+        
+        return self
+    }
+    
+    func apply(_ base: Base) -> Base {
+        return configurations.reduce(base, { $1($0) })
+    }
+}
+
+extension ConfigurationSet where Base: Configurateable {
+    func build() -> Base {
+        return apply(Base())
+    }
+}
