108eff3e5b10bca6dc13c9c831fdaf0b25a5810a
Peter Schumacher <ps@imagineon.de>
2017-07-29 12:26:17 +0200

    extended motionEffets configuration

--- Sources/Core/UIExtensions/UIView.swift
+++ Sources/Core/UIExtensions/UIView.swift
@@ -100,12 +100,17 @@ extension ConfigurationSet where Base: UIView {
     }
 
     @available(iOS 3.2, *)
-    func gestureRecognizers(_ gestureRecognizers: UIGestureRecognizer...) -> ConfigurationSet<Base> {
-        return self.gestureRecognizers(gestureRecognizers)
+    func additionalGestureRecognizer(_ gestureRecognizer: UIGestureRecognizer) -> ConfigurationSet<Base> {
+        return self.additionalGestureRecognizers(gestureRecognizer)
     }
 
     @available(iOS 3.2, *)
-    func gestureRecognizers(_ gestureRecognizers: [UIGestureRecognizer]) -> ConfigurationSet<Base> {
+    func additionalGestureRecognizers(_ gestureRecognizers: UIGestureRecognizer...) -> ConfigurationSet<Base> {
+        return self.additionalGestureRecognizers(gestureRecognizers)
+    }
+
+    @available(iOS 3.2, *)
+    func additionalGestureRecognizers(_ gestureRecognizers: [UIGestureRecognizer]) -> ConfigurationSet<Base> {
         return set { (view: UIView) in
             if view.gestureRecognizers != nil {
                 view.gestureRecognizers?.append(contentsOf: gestureRecognizers)
@@ -169,6 +174,23 @@ extension ConfigurationSet where Base: UIView {
     func motionEffects(_ effects: [UIMotionEffect]) -> ConfigurationSet<Base> {
         return set { (view: UIView) in
             view.motionEffects = effects
+        }
+    }
+
+    @available(iOS 7.0, *)
+    func additionalMotionEffect(_ effect: UIMotionEffect) -> ConfigurationSet<Base> {
+        return self.additionalMotionEffects([effect])
+    }
+
+    @available(iOS 7.0, *)
+    func additionalMotionEffects(_ effects: UIMotionEffect...) -> ConfigurationSet<Base> {
+        return self.additionalMotionEffects(effects)
+    }
+
+    @available(iOS 7.0, *)
+    func additionalMotionEffects(_ effects: [UIMotionEffect]) -> ConfigurationSet<Base> {
+        return set { (view: UIView) in
+            view.motionEffects.append(contentsOf: effects)
         }
     }
 
