786cf6ea6591af3afe1ae466609048cf5569653b
Peter Schumacher <ps@imagineon.de>
2017-08-30 13:29:12 +0200

    Added UILabel Configuration and tests

---
+++ Sources/UILabel.swift
@@ -0,0 +1,112 @@
+import UIKit
+
+public extension ConfigurationSet where Base: UILabel {
+    func text(_ text: String) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.text = text
+        }
+    }
+
+    func font(_ font: UIFont) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.font = font
+        }
+    }
+
+    func textColor(_ textColor: UIColor) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.textColor = textColor
+        }
+    }
+
+    func shadowColor(_ shadowColor: UIColor) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.shadowColor = shadowColor
+        }
+    }
+
+    func shadowOffset(_ shadowOffset: CGSize) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.shadowOffset = shadowOffset
+        }
+    }
+
+    func textAlignment(_ textAlignment: NSTextAlignment) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.textAlignment = textAlignment
+        }
+    }
+
+    func lineBreakMode(_ lineBreakMode: NSLineBreakMode) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.lineBreakMode = lineBreakMode
+        }
+    }
+
+    func attributedText(_ attributedText: NSAttributedString) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.attributedText = attributedText
+        }
+    }
+
+    func highlightedTextColor(_ highlightedTextColor: UIColor) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.highlightedTextColor = highlightedTextColor
+        }
+    }
+
+    func isHighlighted(_ highlighted: Bool) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.isHighlighted = highlighted
+        }
+    }
+
+    func isUserInteractionEnabled(_ userInteractionEnabled: Bool) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.isUserInteractionEnabled = userInteractionEnabled
+        }
+    }
+
+    func isEnabled(_ enabled: Bool) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.isEnabled = enabled
+        }
+    }
+
+    func numberOfLines(_ numberOfLines: Int) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.numberOfLines = numberOfLines
+        }
+    }
+
+    func adjustsFontSizeToFitWidth(_ adjust: Bool) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.adjustsFontSizeToFitWidth = adjust
+        }
+    }
+
+    func baselineAdjustment(_ baselineAdjustment: UIBaselineAdjustment) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.baselineAdjustment = baselineAdjustment
+        }
+    }
+
+    func minimumScaleFactor(_ minimumScaleFactor: CGFloat) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.minimumScaleFactor = minimumScaleFactor
+        }
+    }
+
+    @available(iOSApplicationExtension 9.0, *)
+    func allowsDefaultTighteningForTruncation(_ allowsDefaultTighteningForTruncation: Bool) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.allowsDefaultTighteningForTruncation = allowsDefaultTighteningForTruncation
+        }
+    }
+
+    func preferredMaxLayoutWidth(_ preferredMaxLayoutWidth: CGFloat) -> ConfigurationSet<Base> {
+        return set { (label: UILabel) in
+            label.preferredMaxLayoutWidth = preferredMaxLayoutWidth
+        }
+    }
+}
---
+++ Tests/UILabelSpecs.swift
@@ -0,0 +1,169 @@
+import Quick
+import Nimble
+import ViewConfigurator
+
+class UILabelSpec: QuickSpec {
+    
+    override func spec() {
+        describe("UIViewConfigurator") {
+            it("can set Text Value") {
+                let text = "Test Text"
+                let testView: UILabel = .build { set in
+                    set.text(text)
+                }
+                expect(testView.text).to(equal(text))
+            }
+            it("can set Font Value") {
+                let font = UIFont.boldSystemFont(ofSize: 30)
+                let testView: UILabel = .build { set in
+                    set.font(font)
+                }
+                expect(testView.font).to(equal(font))
+            }
+            it("can set textColor Value") {
+                let color = UIColor.red
+                let testView: UILabel = .build { set in
+                    set.textColor(color)
+                }
+                expect(testView.textColor).to(equal(color))
+            }
+            it("can set shadowColor Value") {
+                let color = UIColor.red
+                let testView: UILabel = .build { set in
+                    set.shadowColor(color)
+                }
+                expect(testView.shadowColor).to(equal(color))
+            }
+            it("can set shadowOffset Value") {
+                let offset = CGSize(width: 23, height: 42)
+                let testView: UILabel = .build { set in
+                    set.shadowOffset(offset)
+                }
+                expect(testView.shadowOffset).to(equal(offset))
+            }
+            it("can set textAlignment Value") {
+                let alignment: NSTextAlignment = .center
+                let testView: UILabel = .build { set in
+                    set.textAlignment(alignment)
+                }
+                expect(testView.textAlignment).to(equal(alignment))
+            }
+            it("can set attributedText Value") {
+                let attributedText: NSAttributedString = NSAttributedString(string: "Test Attributed String")
+                let testView: UILabel = .build { set in
+                    set.attributedText(attributedText)
+                }
+                expect(testView.attributedText).to(equal(attributedText))
+            }
+            it("can set highlightedTextColor Value") {
+                let highlightedTextColor: UIColor = .red
+                let testView: UILabel = .build { set in
+                    set.highlightedTextColor(highlightedTextColor)
+                }
+                expect(testView.highlightedTextColor).to(equal(highlightedTextColor))
+            }
+            it("can set isHighlighted Value to true") {
+                let highlighted = true
+                let testView: UILabel = .build { set in
+                    set.isHighlighted(highlighted)
+                }
+                expect(testView.isHighlighted).to(equal(highlighted))
+            }
+            it("can set isHighlighted Value to false") {
+                let highlighted = false
+                let testView: UILabel = .build { set in
+                    set.isHighlighted(highlighted)
+                }
+                expect(testView.isHighlighted).to(equal(highlighted))
+            }
+            it("can set isUserInteractionEnabled Value to true") {
+                let enabled = true
+                let testView: UILabel = .build { set in
+                    set.isUserInteractionEnabled(enabled)
+                }
+                expect(testView.isUserInteractionEnabled).to(equal(enabled))
+            }
+            it("can set isUserInteractionEnabled Value to false") {
+                let enabled = false
+                let testView: UILabel = .build { set in
+                    set.isUserInteractionEnabled(enabled)
+                }
+                expect(testView.isUserInteractionEnabled).to(equal(enabled))
+            }
+            it("can set isEnabled Value to true") {
+                let enabled = true
+                let testView: UILabel = .build { set in
+                    set.isEnabled(enabled)
+                }
+                expect(testView.isEnabled).to(equal(enabled))
+            }
+            it("can set isEnabled Value to false") {
+                let enabled = false
+                let testView: UILabel = .build { set in
+                    set.isEnabled(enabled)
+                }
+                expect(testView.isEnabled).to(equal(enabled))
+            }
+            it("can set isEnabled Value to false") {
+                let enabled = false
+                let testView: UILabel = .build { set in
+                    set.isEnabled(enabled)
+                }
+                expect(testView.isEnabled).to(equal(enabled))
+            }
+            it("can set adjustsFontSizeToFitWidth Value to false") {
+                let adjustsFontSizeToFitWidth = false
+                let testView: UILabel = .build { set in
+                    set.adjustsFontSizeToFitWidth(adjustsFontSizeToFitWidth)
+                }
+                expect(testView.adjustsFontSizeToFitWidth).to(equal(adjustsFontSizeToFitWidth))
+            }
+            it("can set adjustsFontSizeToFitWidth Value to true") {
+                let adjustsFontSizeToFitWidth = true
+                let testView: UILabel = .build { set in
+                    set.adjustsFontSizeToFitWidth(adjustsFontSizeToFitWidth)
+                }
+                expect(testView.adjustsFontSizeToFitWidth).to(equal(adjustsFontSizeToFitWidth))
+            }
+            it("can set baselineAdjustment Value") {
+                let baselineAdjustment: UIBaselineAdjustment = .alignCenters
+                let testView: UILabel = .build { set in
+                    set.baselineAdjustment(baselineAdjustment)
+                }
+                expect(testView.baselineAdjustment).to(equal(baselineAdjustment))
+
+            }
+            it("can set minimumScaleFactor Value") {
+                let minimumScaleFactor: CGFloat = 0.5
+                let testView: UILabel = .build { set in
+                    set.minimumScaleFactor(minimumScaleFactor)
+                }
+                expect(testView.minimumScaleFactor).to(equal(minimumScaleFactor))
+            }
+            if #available(iOS 9.0, *) {
+                it("can set allowsDefaultTighteningForTruncation Value to true") {
+                    let allowsDefaultTighteningForTruncation = true
+                    let testView: UILabel = .build { set in
+                        set.allowsDefaultTighteningForTruncation(allowsDefaultTighteningForTruncation)
+                    }
+                    
+                    expect(testView.allowsDefaultTighteningForTruncation).to(equal(allowsDefaultTighteningForTruncation))
+                }
+                it("can set allowsDefaultTighteningForTruncation Value to false") {
+                    let allowsDefaultTighteningForTruncation = false
+                    let testView: UILabel = .build { set in
+                        set.allowsDefaultTighteningForTruncation(allowsDefaultTighteningForTruncation)
+                    }
+                    expect(testView.allowsDefaultTighteningForTruncation).to(equal(allowsDefaultTighteningForTruncation))
+                }
+            }
+            it("can set preferredMaxLayoutWidth Value") {
+                let preferredMaxLayoutWidth: CGFloat = 23
+                let testView: UILabel = .build { set in
+                    set.preferredMaxLayoutWidth(preferredMaxLayoutWidth)
+                }
+                expect(testView.preferredMaxLayoutWidth).to(equal(preferredMaxLayoutWidth))
+            }
+        }
+    }
+}
