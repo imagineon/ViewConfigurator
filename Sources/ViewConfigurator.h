b6c9c1c8a97015d9bfb944c285cc480cd2ef1993
Peter Schumacher <ps@imagineon.de>
2017-08-30 10:31:10 +0200

    fixed some rename issues

--- Sources/ViewConfigurator.h
+++ Sources/ViewConfigurator.h
@@ -6,4 +6,4 @@ FOUNDATION_EXPORT double ConfiguratorVersionNumber;
 //! Project version string for Configurator.
 FOUNDATION_EXPORT const unsigned char ConfiguratorVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Configurator/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <ViewConfigurator/PublicHeader.h>
