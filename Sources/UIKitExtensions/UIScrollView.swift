3e8b8c50f19b4ea1c4734058034979d17d757cc2
Tomas Silveira Salles <ts@imagineon.de>
2018-01-02 17:13:39 +0100

    UIScrollView: Implemented second half of the relevant methods.

--- Sources/UIKitExtensions/UIScrollView.swift
+++ Sources/UIKitExtensions/UIScrollView.swift
@@ -79,4 +79,83 @@ public extension ConfigurationSet where Base: UIScrollView {
             scrollView.decelerationRate = decelerationRate
         }
     }
+    
+    func indicatorStyle(_ indicatorStyle: UIScrollViewIndicatorStyle) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.indicatorStyle = indicatorStyle
+        }
+    }
+    
+    func scrollIndicatorInsets(_ scrollIndicatorInsets: UIEdgeInsets) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.scrollIndicatorInsets = scrollIndicatorInsets
+        }
+    }
+    
+    func showsVerticalScrollIndicator(_ showsVerticalScrollIndicator: Bool) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.showsVerticalScrollIndicator = showsVerticalScrollIndicator
+        }
+    }
+    
+    func showsHorizontalScrollIndicator(_ showsHorizontalScrollIndicator: Bool) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.showsHorizontalScrollIndicator = showsHorizontalScrollIndicator
+        }
+    }
+    
+    @available(iOS 10.0, *)
+    func refreshControl(_ refreshControl: UIRefreshControl?) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.refreshControl = refreshControl
+        }
+    }
+    
+    func canCancelContentTouches(_ canCancelContentTouches: Bool) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.canCancelContentTouches = canCancelContentTouches
+        }
+    }
+    
+    func delaysContentTouches(_ delaysContentTouches: Bool) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.delaysContentTouches = delaysContentTouches
+        }
+    }
+    
+    func zoomScale(_ zoomScale: CGFloat) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.zoomScale = zoomScale
+        }
+    }
+    
+    func minimumZoomScale(_ minimumZoomScale: CGFloat) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.minimumZoomScale = minimumZoomScale
+        }
+    }
+    
+    func maximumZoomScale(_ maximumZoomScale: CGFloat) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.maximumZoomScale = maximumZoomScale
+        }
+    }
+    
+    func bouncesZoom(_ bouncesZoom: Bool) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.bouncesZoom = bouncesZoom
+        }
+    }
+    
+    func keyboardDismissMode(_ keyboardDismissMode: UIScrollViewKeyboardDismissMode) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.keyboardDismissMode = keyboardDismissMode
+        }
+    }
+    
+    func indexDisplayMode(_ indexDisplayMode: UIScrollViewIndexDisplayMode) -> ConfigurationSet<Base> {
+        return set { (scrollView: Base) in
+            scrollView.indexDisplayMode = indexDisplayMode
+        }
+    }
 }
