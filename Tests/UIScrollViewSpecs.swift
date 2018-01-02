f00947b8284b000f879f235b7dfcdc41f4eda075
Tomas Silveira Salles <ts@imagineon.de>
2018-01-02 17:36:54 +0100

    UIScrollView: Added test class.

---
+++ Tests/UIScrollViewSpecs.swift
@@ -0,0 +1,10 @@
+import Quick
+import Nimble
+import ViewConfigurator
+
+class UIScrollViewSpecs: QuickSpec {
+    override func spec() {
+        describe("UIViewConfigurator") {
+        }
+    }
+}
