a5a724c1b455fd2ee9e905f0a3826a798719fdce
Peter Schumacher <ps@imagineon.de>
2018-04-19 11:16:50 +0200

    added generated swiftlint disable

--- Sources/UIKitExtensionsSorceryGeneration/UIAlertView+SubAndSuperclassInterop.swift
+++ Sources/UIKitExtensionsSorceryGeneration/UIAlertView+SubAndSuperclassInterop.swift
@@ -1,6 +1,8 @@
 // Generated using Sourcery 0.12.0 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 
+// swiftlint:disable line_length identifier_name
+
 import UIKit
 
 extension Configurable where Self: UIAlertView {
